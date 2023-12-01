1133c0154646201f2dbdccf83492bc56b6df463f
Habibur Rahman <habibur.rahman@dreamonline.co.jp>
2023-12-01 12:16:01 +0600

    Add asistant Gallery scene

---
+++ Assets/Scripts/Data/AssistantData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class AssistantData 
+{
+    public string title;
+    public string subTitle;
+    public string assistantId;
+    public Sprite thumbImage;
+}
---
+++ Assets/Scripts/UI/AssistantItem.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class AssistantItem : MonoBehaviour
+{
+
+    public TMP_Text title;
+    public TMP_Text subTitle;
+    public Image image;
+
+}
